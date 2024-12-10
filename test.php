12322c95deb6588fac2c7c5894655fe86d4fe74a
Rasel <rabbiislamrasel01@gmail.com>
2024-12-10 17:42:46 +0600

    test

---
+++ test.php
@@ -0,0 +1,19 @@
+<?php
+ echo ("Hello WOrld!"."\n");
+ 
+ $age = 20;
+//  $word = "age";
+//  echo $$word;
+ $name = "Bangladesh";
+ $qustion = "How are you";
+ echo $name ." " .$qustion ."\n";
+ echo("Hello $name \n");
+ echo("Hello {$name},{$qustion}");
+ echo("\n");
+
+ echo ($age >= 18)?"Adult":"child";
+ function myFunction($a , $b){
+    $result = $a + $b;
+    echo "The result is : ".$result;
+ }
+ myFunction(5,13);
